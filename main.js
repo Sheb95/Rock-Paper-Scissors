7c03890f8feb694c1e49af5701610d85003c2b66
marian.eq <mariano.eq@gmail.com>
2021-11-19 10:47:02 +0000

    Paper's movements

--- main.js
+++ main.js
@@ -4,8 +4,12 @@
 let playerMove = "rock";
 let computerMove = "paper";
 
-//playerMove = rock
-if(playerMove === 'rock' && computerMove === 'paper'){
-    console
+//playerMove = paper
+if(playerMove === 'paper' && computerMove === 'scissors'){
+    console.log("computer Wins!!");
+} else if(playerMove === 'scissors' && computerMove === 'paper'){
+    console.log("player Wins")
+} else{
+    console.log("It's a Draw!")
 }
 
