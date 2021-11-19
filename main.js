64ec8795f0a22ce6b2424dbd0ccfa3b24c948db6
marian.eq <mariano.eq@gmail.com>
2021-11-19 11:08:00 +0000

    saving main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 let playerMove = "rock";
 let computerMove = "paper";
 
-<<<<<<< HEAD
 //playerMove = paper
 if(playerMove === 'paper' && computerMove === 'scissors'){
     console.log("computer Wins!!");
@@ -13,16 +12,4 @@ if(playerMove === 'paper' && computerMove === 'scissors'){
 } else{
     console.log("It's a Draw!")
 }
-=======
-//playerMove = rock
-if(playerMove === 'rock' && computerMove === 'paper'){
-    console.log('Computer Wins!');
-}else if(playerMove === 'rock' && computerMove === 'scissors'){
-    console.log('Player wins!');
-    
-
-}else{
-    console.log("It's a Draw!");
-};
->>>>>>> 89bf03d449d09530cf815578bbb172ff96272021
 
