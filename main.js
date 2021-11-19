89bf03d449d09530cf815578bbb172ff96272021
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2021-11-19 10:40:18 +0000

    player move rock

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ let computerMove = "paper";
 
 //playerMove = rock
 if(playerMove === 'rock' && computerMove === 'paper'){
-    console
-}
+    console.log('Computer Wins!');
+}else if(playerMove === 'rock' && computerMove === 'scissors'){
+    console.log('Player wins!');
+    
+
+}else{
+    console.log("It's a Draw!");
+};
 
